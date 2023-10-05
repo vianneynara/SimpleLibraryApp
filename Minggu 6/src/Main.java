23a20e607a0505975708eb93697e87795a616dae
narwa <narwandaru.nara@gmail.com>
2023-10-05 07:48:21 +0700

    [Trash] Test file

---
+++ Minggu 6/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		String message = "Welcome to Java!";
+//		System.out.println(message.substring(0, 7));
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Input your string: ");
+		String original = in.nextLine();
+
+		System.out.println("Original value				: " + original);
+		System.out.println("Removed spaces at first/end	: " + original.trim());
+		System.out.println("Removed double/more spaces	: " + original.trim().replaceAll(" {2,}", " "));
+	}
+}
