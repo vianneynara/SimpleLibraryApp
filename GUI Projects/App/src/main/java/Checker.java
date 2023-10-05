a8fc58e50de12bd6b0e7411396e89c9942acdf99
narwa <narwandaru.nara@gmail.com>
2023-10-05 07:48:56 +0700

    [Feature] Added checker using regex for numbers and alphabets

--- GUI Projects/App/src/main/java/Checker.java
+++ GUI Projects/App/src/main/java/Checker.java
@@ -8,9 +8,19 @@ public class Checker {
      * @return boolean true atau false
      * */
     public static boolean isNomorSpaceHyphen(String str) {
-        Pattern pola = Pattern.compile("^[0-9\\s\\-]+$");
+        Pattern pattern = Pattern.compile("^[0-9\\s\\-]+$");
+        return pattern.matcher(str).matches();
+    }
 
-        return pola.matcher(str).matches();
+    /**
+     * Mengecek apakah sebuah {@link String} terdiri hanya dari huruf dan spasi.
+     *
+     * @param str {@link String} yang akan dicek
+     * @return boolean true atau false
+     * */
+    public static boolean isAlphabetSpace(String str) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z\\s]{3,}$");
+        return pattern.matcher(str).matches();
     }
 
     /**
@@ -20,8 +30,19 @@ public class Checker {
      * @return boolean true atau false
      * */
     public static boolean isNomor(String str) {
-        Pattern pola = Pattern.compile("^[0-9]+$");
+        Pattern pattern = Pattern.compile("^[0-9]+$");
+        return pattern.matcher(str).matches();
+    }
 
-        return pola.matcher(str).matches();
+    /**
+     * Mengecek apakah sebuah {@link String} terdiri hanya dari angka dan harus sebanyak n digit.
+     *
+     * @param str {@link String} yang akan dicek
+     * @param digits jumlah karakter angka / digit yang sesuai
+     * @return boolean true atau false
+     * */
+    public static boolean isNomor(String str, int digits) {
+        Pattern pattern = Pattern.compile(String.format("^\\d{%d}$", digits));
+        return pattern.matcher(str).matches();
     }
 }
