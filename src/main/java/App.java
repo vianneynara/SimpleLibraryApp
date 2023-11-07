2e7d60b73f78f53ef993964e26b5ea87f6a8f43a
narwa <narwandaru.nara@gmail.com>
2023-11-07 22:10:47 +0700

    Reformat Dosen Umum Mahasiswa

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -71,8 +71,8 @@ public class App extends javax.swing.JFrame {
 	protected void fillPeminjam() {
 		System.out.println("Filling list of customers with test data...");
 		dataPerpus.isiDataPeminjam(new Dosen("D00001", "Dosen1", "NIP", "Paingan Yogyakarta", "012345678911", 3, "123456781234567801"));
-		dataPerpus.isiDataPeminjam(new Dosen("D00001", "Dosen2", "NIP", "Depok Yogyakarta", "012345678912", 1, "123456781234567802"));
-		dataPerpus.isiDataPeminjam(new Dosen("D00001", "Dosen3", "NIP", "Klaten Yogyakarta", "012345678913", 4, "123456781234567803"));
+		dataPerpus.isiDataPeminjam(new Dosen("D00002", "Dosen2", "NIP", "Depok Yogyakarta", "012345678912", 1, "123456781234567802"));
+		dataPerpus.isiDataPeminjam(new Dosen("D00003", "Dosen3", "NIP", "Klaten Yogyakarta", "012345678913", 4, "123456781234567803"));
 		dataPerpus.isiDataPeminjam(new Mahasiswa("M00001", "Mahasiswa1", "NIM", "Bantul Something", "012345678921", 3, "225314001"));
 		dataPerpus.isiDataPeminjam(new Mahasiswa("M00002", "Mahasiswa2", "NIM", "Bekasi Something", "012345678922", 5, "225314002"));
 		dataPerpus.isiDataPeminjam(new Umum("U00001", "Umum1", "NIK", "Kaliurang Something", "012345678930", 5, "2253140021234567"));
