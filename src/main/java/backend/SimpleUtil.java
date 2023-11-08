a183cfcbf00645e42284b145c994f3198638a404
narwa <narwandaru.nara@gmail.com>
2023-11-09 01:02:10 +0700

    Added simple logging class with simple logger methods

---
+++ src/main/java/backend/SimpleUtil.java
@@ -0,0 +1,30 @@
+package backend;
+
+import java.time.LocalTime;
+
+/**
+ * Simple class to log a message with time format.
+ * */
+public class SimpleUtil {
+
+	/* Simple loggers */
+
+	public static void log(String msg) {
+		log(msg, "\n");
+	}
+
+	public static void log(String msg, String endLine) {
+		log(msg, endLine, null);
+	}
+
+	public static void log(String msg, String endLine, Object caller) {
+		final var time = LocalTime.now();
+		if (caller == null) {
+			System.out.printf("%02d:%02d:%02d | LOG : " + msg + endLine,
+				time.getHour(), time.getMinute(), time.getSecond());
+		} else {
+			System.out.printf("@ " + caller.getClass().getName() + "| %02d:%02d:%02d | LOG : " + msg + endLine,
+				time.getHour(), time.getMinute(), time.getSecond());
+		}
+	}
+}
