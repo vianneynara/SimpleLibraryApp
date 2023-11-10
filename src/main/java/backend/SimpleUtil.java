2cf5430bbd8266bd5d2ece501605a7c260ac4a7a
narwa <narwandaru.nara@gmail.com>
2023-11-10 08:24:12 +0700

    Update documentation

--- src/main/java/backend/SimpleUtil.java
+++ src/main/java/backend/SimpleUtil.java
@@ -6,17 +6,27 @@ import java.time.LocalTime;
  * Simple class to log a message with time format.
  * */
 public class SimpleUtil {
-
 	/* Simple loggers */
-
+	/**
+	 * Logs a simple message and its time of execution.
+	 * */
 	public static void log(String msg) {
 		log(msg, "\n");
 	}
 
+	/**
+	 * Logs a message with a defined end line.
+	 * */
 	public static void log(String msg, String endLine) {
 		log(msg, endLine, null);
 	}
 
+	/**
+	 * Logs a message, with its end line and location.
+	 * @param msg The message to be sent.
+	 * @param endLine A string that will be concatenated at the back of the actual message.
+	 * @param caller The class that is assumed as this method's invoker, used to print the location of the log.
+	 * */
 	public static void log(String msg, String endLine, Object caller) {
 		final var time = LocalTime.now();
 		if (caller == null) {
