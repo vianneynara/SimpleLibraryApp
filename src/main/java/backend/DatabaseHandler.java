ee9eeaa56ba789991e7556cb4f6bfdb93b0b1afa
narwa <narwandaru.nara@gmail.com>
2023-11-22 15:53:15 +0700

    Added tipe koleksi database insertion method

--- src/main/java/backend/DatabaseHandler.java
+++ src/main/java/backend/DatabaseHandler.java
@@ -39,9 +39,8 @@ public class DatabaseHandler {
 	public static HashMap<String, Peminjam> readDataPeminjam() {
 		try (Connection conn = getConnection()) {
 			HashMap<String, Peminjam> dataPeminjam = new HashMap<>();
-			String query =
-				/* Memilih kolom atribut yang tertera dari tabel "Peminjam" */
-				"SELECT id_peminjam, nama_lengkap, jenis_no_id, alamat, nomor_telepon, maks_pinjam, nomor_identitas FROM \"Peminjam\"";
+			/* Memilih kolom atribut yang tertera dari tabel "Peminjam" */
+			String query = "SELECT * FROM \"Peminjam\"";
 
 			// Try with resource disini beguna untuk menyiapkan query statement yang akan dijalankan menggunakan
 			// Prepared Statement. Disiapkan dengan koneksi yang sudah terhubung `conn`, dengan query yang sebelumnya
@@ -69,10 +68,58 @@ public class DatabaseHandler {
 						peminjam = new Umum(id_peminjam, nama_lengkap, jenis_no_id, alamat, nomor_telepon, maks_pinjam, nomor_identitas);
 					}
 
-					dataPeminjam.put(id_peminjam, peminjam);// Memasukkan id_peminjam dan objek peminjam ke hash Map
+					dataPeminjam.put(id_peminjam, peminjam);// Memasukkan id_peminjam dan objek peminjam ke hash map
 				}
 			}
 			return dataPeminjam;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Membaca keseluruhan isi dari tabel Koleksi dan mengembalikkannya dalam bentuk hash map.
+	 * @return {@link HashMap} berisi data koleksi dengan id_koleksi sebagai key dan objek {@link Koleksi}
+	 * sebagai value.
+	 */
+	public static HashMap<String, Koleksi> readDataKoleksi() {
+		try (Connection conn = getConnection()) {
+			HashMap<String, Koleksi> dataKoleksi = new HashMap<>();
+			/* Memilih kolom atribut yang tertera dari tabel "Peminjam" */
+			String query = "SELECT * FROM \"Koleksi\"";
+
+			try (PreparedStatement ps = conn.prepareStatement(query);
+				 ResultSet rs = ps.executeQuery()) {
+				while (rs.next()) {							// Selama masih ada nilai untuk setelahnya
+					/* Mengambil atribut pada tabel dan menyimpannya ke variabel sementara */
+					String id_koleksi 		= rs.getString("id_koleksi");
+					String judul 			= rs.getString("judul");
+					String penerbit 		= rs.getString("penerbit");
+					String tahunTerbit		= rs.getString("tahun_terbit");
+//					int status_pinjam  		= rs.getInt("status_pinjam");
+					String tipe 			= rs.getString("tipe");
+					String isbn_issn		= rs.getString("isbn_issn");
+
+					Koleksi koleksi;
+					/* Membuat objek inheritor Peminjam sesuai dengan tipe */
+					switch (tipe) {
+						case "BUKU" -> {
+							koleksi = new Buku(id_koleksi, judul, penerbit, tahunTerbit, isbn_issn,
+								rs.getInt("jumlah_halaman"));
+						}
+						case "MAJALAH" -> {
+							koleksi = new Majalah(id_koleksi, judul, penerbit, tahunTerbit, isbn_issn,
+								rs.getInt("volume"), rs.getInt("seri"));
+						}
+						default -> {
+							koleksi = new Disk(id_koleksi, judul, penerbit, tahunTerbit, isbn_issn,
+								rs.getString("format"));
+						}
+					}
+					dataKoleksi.put(id_koleksi, koleksi);// Memasukkan id_koleksi dan objek peminjam ke hash map
+				}
+			}
+			return dataKoleksi;
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
@@ -104,6 +151,50 @@ public class DatabaseHandler {
 				ps.setString(7, d.getNip());
 			} else {
 				ps.setString(7, ((Umum) data).getNik());
+			}
+
+			ps.executeUpdate();									// Melakukan update menggunakan prepared statement
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Memasukkan data bertipe {@link Koleksi} dan memasukkan atribut-atribut objeknya menjadi atribut record dalam
+	 * database.
+	 * */
+	public static void inserDataKoleksi(Koleksi data) {
+		String query =
+			/* Memasukkan ke dalam Peminjam pada kolom (yang tertera) dengan values (sesuai dengan jumlah kolom)*/
+			"INSERT INTO " +
+				"\"Koleksi\" (id_koleksi, judul, penerbit, tahun_terbit, status_pinjam, tipe, isbn_issn, jumlah_halaman, volume, seri, format) " +
+				"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		try (Connection conn = getConnection();
+			 PreparedStatement ps = conn.prepareStatement(query)) {
+			/* Melakukan pengisian template prepared statement dengan data yang relevan */
+			ps.setString(1, data.getIdKoleksi());
+			ps.setString(2, data.getJudul());
+			ps.setString(3, data.getPenerbit());
+			ps.setString(4, data.tahunTerbit);
+			ps.setInt	(5, data.statusPinjam ? 1 : 0);
+			ps.setString(6, data.getTipe());
+
+			/* Handling values yang mungkin kosong sesuai tipenya */
+			ps.setInt(8, 0);
+			ps.setInt(9, 0);
+			ps.setInt(10, 0);
+			ps.setString(11, null);
+
+			if (data instanceof Buku b) {
+				ps.setString(7, b.getIsbn());
+				ps.setInt(8, b.getJumlahHalaman());
+			} else if (data instanceof Majalah m) {
+				ps.setString(7, m.getIssn());
+				ps.setInt(9, m.getVolume());
+				ps.setInt(10, m.getSeri());
+			} else {
+				ps.setString(7, ((Disk) data).getIsbn());
+				ps.setString(11, ((Disk) data).getFormat());
 			}
 
 			ps.executeUpdate();									// Melakukan update menggunakan prepared statement
