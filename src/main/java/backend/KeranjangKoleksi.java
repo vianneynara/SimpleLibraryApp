fcc84e8afa121805956111ba2eace5e8d689b09d
narwa <narwandaru.nara@gmail.com>
2023-12-05 11:18:50 +0700

    docs: added and improved docs

--- src/main/java/backend/KeranjangKoleksi.java
+++ src/main/java/backend/KeranjangKoleksi.java
@@ -9,6 +9,20 @@ import javax.swing.table.DefaultTableModel;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Kelas yang merepresentasikan keranjang koleksi yang dipinjam. Kelas ini akan menyimpan koleksi yang akan dipinjam
+ * oleh peminjam. Metode yang terdapat pada kelas ini adalah:
+ * <ul>
+ *     <li>{@link #getTableModel()}</li>
+ *     <li>{@link #addKoleksi(Koleksi)}</li>
+ *     <li>{@link #contains(Koleksi)}</li>
+ *     <li>{@link #getPreparedKoleksiArray()}</li>
+ *     <li>{@link #reset()}</li>
+ *     <li>{@link #size()}</li>
+ *     <li>{@link #isEmpty()}</li>
+ *     <li>{@link #getArrayKoleksi()}</li>
+ * </ul>
+ * */
 public class KeranjangKoleksi {
 	
 	private final List<Koleksi> arrayKoleksi;
@@ -28,7 +42,9 @@ public class KeranjangKoleksi {
 	}
 
 	/**
-	 * Mendapatkan array data koleksi untuk membuat tabel sesuai dengan kolom yang ada.
+	 * Mendapatkan {@link DefaultTableModel} koleksi untuk membuat tabel sesuai dengan kolom yang ada.
+	 *
+	 * @return {@link DefaultTableModel} yang berisi data koleksi yang ada di keranjang.
 	 * */
 	public DefaultTableModel getTableModel() {
 		// Mengecek apabila tidak ada koleksi di keranjang. Mengembalikan tabel kosong
@@ -39,6 +55,7 @@ public class KeranjangKoleksi {
             );
 		}
 
+		// Membuat tabel dengan data koleksi yang ada di keranjang
 		Object[][] data = new Object[arrayKoleksi.size()][4];
 		String[] columns = {"id", "title", "isbn/issn", "tipe"};
 		for (int i = 0; i < arrayKoleksi.size(); i++) {
@@ -63,6 +80,9 @@ public class KeranjangKoleksi {
 
 	/**
 	 * Menambahkan koleksi ke keranjang.
+	 *
+	 * @param koleksi Koleksi yang akan ditambahkan ke keranjang.
+	 * @return boolean true atau false
 	 * */
 	public boolean addKoleksi(Koleksi koleksi) {
 		// Mengecek apabila sudah terdapat koleksi di keranjang.
@@ -75,11 +95,18 @@ public class KeranjangKoleksi {
 
 	/**
 	 * Mengecek apabila sudah terdapat koleksi {@code koleksi} di keranjang.
+	 *
+	 * @param koleksi Koleksi yang akan dicek.
+	 * @return boolean true atau false
 	 * */
 	public boolean contains(Koleksi koleksi) {
 		return arrayKoleksi.contains(koleksi);
 	}
 
+	/**
+	 * Melakukan pengembalian array koleksi yang ada di keranjang. Metode ini akan mengubah status koleksi yang ada
+	 * di keranjang menjadi dipinjam.
+	 * */
 	public Koleksi[] getPreparedKoleksiArray() {
 		for (Koleksi koleksi : arrayKoleksi) {
 			koleksi.setDipinjam();
@@ -97,14 +124,23 @@ public class KeranjangKoleksi {
 		arrayKoleksi.clear();
 	}
 
+	/**
+	 * Mendapatkan jumlah koleksi yang ada di array koleksi keranjang.
+	 * */
 	public int size() {
 		return arrayKoleksi.size();
 	}
 
+	/**
+	 * Mengembalikan apakah keranjang kosong atau tidak.
+	 * */
 	public boolean isEmpty() {
 		return arrayKoleksi.isEmpty();
 	}
 
+	/**
+	 * Mendapatkan array koleksi yang ada di keranjang.
+	 * */
 	public List<Koleksi> getArrayKoleksi() {
 		return arrayKoleksi;
 	}
